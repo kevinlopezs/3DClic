50e1788ec50e517fcb284d07920990070dc91e61
kevin <kevinlopezs15@gmail.com>
2024-01-21 21:46:32 -0500

    "ok"

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  //ok
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Hive.initFlutter();
